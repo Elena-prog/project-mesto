f273b8ab5a8ab677d782c097491677bb1e6fec9a
Elena Tsivinskaya <elts77@gmail.com>
2022-06-14 20:16:54 +0700

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let editButton = document.querySelector('.edit-button'),
+    popup = document.querySelector('.popup'),
+    closePopup = document.querySelector('.popup__close-button'),
+    name = document.querySelector('.profile__name'),
+    description = document.querySelector('.profile__description'),
+    inputName = document.querySelector('.popup__input_type_name'),
+    inputDescription = document.querySelector('.popup__input_type_description'),
+    submitButton = document.querySelector('.popup__submit-button');
+
+
+function editProfile () {
+    popup.classList.add('popup_opened');
+    inputName.value = name.textContent;
+    inputDescription.value = description.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    name.textContent = inputName.value;
+    description.textContent = inputDescription.value;
+}
+
+function close () {
+    popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', editProfile);
+
+popup.addEventListener('submit', formSubmitHandler);
+
+closePopup.addEventListener('click', close);
+
+submitButton.addEventListener('click', close);
+
