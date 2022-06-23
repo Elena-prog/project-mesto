49e773d8b4902eb9b174f65a0ada00f3cbd7919e
Elena Tsivinskaya <elts77@gmail.com>
2022-06-23 14:50:27 +0700

    added cards massiv

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,50 @@ const editButton = document.querySelector('.edit-button'),
     name = document.querySelector('.profile__name'),
     description = document.querySelector('.profile__description'),
     inputName = document.querySelector('.popup__input_type_name'),
-    inputDescription = document.querySelector('.popup__input_type_description');
+    inputDescription = document.querySelector('.popup__input_type_description'),
+    cardsContainer = document.querySelector('.elements__group');
+
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+initialCards.forEach(function(item){
+    const cardTemplate = document.querySelector('#card-template').content,
+        card = cardTemplate.querySelector('.element').cloneNode(true),
+        cardName = card.querySelector('.element__title'),
+        cardImage = card.querySelector('.element__image');
+
+    cardName.textContent = item.name;
+    cardImage.setAttribute('src', item.link);
+    
+
+    cardsContainer.append(card);
+})
 
 function editProfile () {
     popup.classList.add('popup_opened');
@@ -28,31 +71,4 @@ editButton.addEventListener('click', editProfile);
 popup.addEventListener('submit', formSubmitHandler);
 
 closePopup.addEventListener('click', close);
-
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
 
